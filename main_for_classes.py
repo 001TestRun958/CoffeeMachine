34e4000f2a96c5cbeefa04acc5db23eda245b464
Helena Sekacova <97595522+001TestRun958@users.noreply.github.com>
2022-05-31 11:28:09 +0200

    OOP

---
+++ main_for_classes.py
@@ -0,0 +1,21 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu=Menu()
+coffee_maker=CoffeeMaker() 
+money_machine=MoneyMachine()
+
+device_on=True
+
+while device_on:
+    choice=input(f"What would you like to drink? {menu.get_items()}: ")
+    if choice=="off":
+        device_on=False
+    elif choice=="report":
+        print(coffee_maker.report())
+        print(money_machine.report())
+    else:
+        drink=menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
